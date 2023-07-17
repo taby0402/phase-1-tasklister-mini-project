88b4a5734db7151be14c04f335329edd19bd87eb
Tabitha <flor.palida@yahoo.com>
2023-07-17 10:02:39 -0400

    corrected errors

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+const createTaskForm = document.getElementById("create-task-form");
+createTaskForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  
+  const newTaskInput = document.getElementById("new-task-description");
+  const newTask = newTaskInput.value;
+  
+  const listItem = document.createElement("li");
+  listItem.textContent = newTask;
+  
+  const list = document.getElementById('list');
+  list.appendChild(listItem);
+  
+  console.log(newTask);
+  
